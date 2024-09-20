cedeb40203feebe513720fa3476d877759220d09
Paul Sant <45962682+psant01@users.noreply.github.com>
2024-09-20 14:24:44 +0100

    Add files via upload
    A Python program that implements the looping construct and works through each element in a data structure

---
+++ simpleLoop.py
@@ -0,0 +1,16 @@
+# A simple Python program that introduces the concept of loops
+# Data structures, data modelling and computer systems
+# The University of Law
+# Dr Paul Sant
+# July 2024
+
+result = 0 # This variable will hold the result of our program
+currentInput = 0 #This variable will hold the value of the current number entered
+
+# Loop 10 times and each time add the number entered to the result
+for in in range(1,11):
+  currentInput = int(input("Please enter a number:")
+  result = result + currentInput
+
+# Print out the result
+print("The final result is: ", result)
